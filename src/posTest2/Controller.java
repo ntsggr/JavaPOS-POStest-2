8f9261078fad89d095a354bccd441413e1600034
Markus Weissenbek <mwk@ntswincash.com>
2013-07-24 09:45:42 +0200

    Adding the Controller

--- src/posTest2/Controller.java
+++ src/posTest2/Controller.java
@@ -1,6 +1,9 @@
 package posTest2;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import javafx.application.Application;
 import javafx.beans.InvalidationListener;
@@ -14,30 +17,52 @@ import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class About extends Application {
+public class Controller extends Application {
 
 	@FXML
-	private TableView deviceTable;
+	private TableView<Device> deviceTable = new TableView<Device>();
 	
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		//ObservableList<Device> data = FXCollections.<Device>sequence()
 		
+		ArrayList<Device> al = new ArrayList<Device>();
+		Device d = new Device("Abc", "Abc", "Abc", "Abc");
+		al.add(d);
+		ObservableList<Device> data = FXCollections.observableList(al);
+		
+		deviceTable.setItems(data);
 		//ObservableList l = new 
 		//deviceTable.setItems(new ObservableList("abc","abc","abc","abc"));
 		
 		Parent root = FXMLLoader.load(getClass().getResource("gui/About.fxml"));
-	    
+		ObservableList ol =root.getChildrenUnmodifiable();
+		Group rootGroup = new Group();
+		
+		VBox vBox = new VBox();
+        vBox.getChildren().add(deviceTable);
+		
+		
+		
         Scene scene = new Scene(root, 1000, 800);
+        
+        //scene.addToDirtyLayoutList(rootGroup);
         
         primaryStage.setTitle("JavaPOS POStest 2");
         
         primaryStage.setScene(scene);
+        
+        
+        
+        
+        
         primaryStage.show();
 	}
 
